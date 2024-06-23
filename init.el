8bb8b1f288d674ce5cea078780d8953e3466cf76
Gemone <git@gemo.one>
2024-06-23 22:26:31 +0800

    update config: add evil mode and magit

--- init.el
+++ init.el
@@ -1,22 +1,23 @@
 
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1) ; Disable visible scroolbar
-(tool-bar-mode -1) ; Disable the toolbar
-(tooltip-mode -1) ; Disable tooltips
-(set-fringe-mode -1) ; Give some breathing room
+(scroll-bar-mode -1)			; Disable visible scroolbar
+(tool-bar-mode -1)			; Disable the toolbar
+(tooltip-mode -1)			; Disable tooltips
+(set-fringe-mode -1)			; Give some breathing room
 
-(menu-bar-mode -1) ; Disable the menu bar
+(menu-bar-mode -1)			; Disable the menu bar
 
 ;; Set up the visible bell
 (setq visible-bell t)
 
 (set-face-attribute 'default nil :font "Fira Code Nerd Font" :height 128)
 
-(load-theme 'tango-dark)
+(load-theme 'wombat)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
 
 ;;; Package manager
 (defvar bootstrap-version)
@@ -39,11 +40,33 @@
 ;; Configure use-package to use straight.el by default
 (setq straight-use-package-by-default t)
 
+;; theme
+(use-package catppuccin-theme
+  :config
+  (load-theme 'catppuccin :no-confirm)
+  :init
+  (setq catppuccin-flavor 'mocha))
+
+;; base config
+(column-number-mode)
+(global-display-line-numbers-mode)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 ;; command log
 (use-package command-log-mode)
 
+(use-package counsel
+  :bind (("C-x b" . counsel-ibuffer)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
 (use-package ivy
-  :diminish
+  :diminish ivy-mode
   :bind (("C-s" . swiper)
 	 :map ivy-minibuffer-map
 	 ("TAB" . ivy-alt-done)
@@ -53,12 +76,32 @@
 	 :map ivy-switch-buffer-map
 	 ("C-k" . ivy-previous-line)
 	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-siwtch-buffer-kill)
+	 ("C-d" . ivy-switch-buffer-kill)
 	 :map ivy-reverse-i-search-map
 	 ("C-k" . ivy-previous-line)
 	 ("C-d" . ivy-reverse-i-search-kill))
   :init
-  (ivy-mode 1))
+  (ivy-mode 1)
+  (counsel-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
 
 (use-package doom-modeline
   :init
@@ -66,5 +109,82 @@
   :custom
   ((doom-modeline-height 15)))
 
-(provide 'init)
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
+(use-package which-key
+  :init
+  (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rune/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")
+    "/" '(counsel-rg :which-key "use rg find")
+    )
+  )
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;; should use it to use shell delete key
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/work/codes/projects")
+    (setq projectile-project-search-path '("~/work/codes/projects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :after magit)
+
+(provide 'init)
