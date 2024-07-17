328d8a920f463ef0c1e34d13fcaa7a80b542f310
Gemone <git@gemo.one>
2024-07-17 22:40:36 +0800

    update config: move config to README.org

--- init.el
+++ init.el
@@ -1,25 +1,28 @@
+;; Automatically tangle our Emacs.org config file when we save it
+(defun emaconf/org-babel-tangle ()
+  (when (string-equal (file-name-directory (buffer-file-name))
+                      (expand-file-name user-emacs-directory))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
 
-(setq inhibit-startup-message t)
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'emaconf/org-babel-tangle)))
 
-(scroll-bar-mode -1)			; Disable visible scroolbar
-(tool-bar-mode -1)			; Disable the toolbar
-(tooltip-mode -1)			; Disable tooltips
-(set-fringe-mode -1)			; Give some breathing room
+(defvar emaconf/default-font-size 180)
+(defvar emaconf/default-variable-font-size 180)
 
-(menu-bar-mode -1)			; Disable the menu bar
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
 
-;; Set up the visible bell
-(setq visible-bell t)
+(defun emaconf/display-startup-time ()
+  (message "Emacs loaded in %s with %d garbage collections."
+           (format "%.2f seconds"
+                   (float-time
+                    (time-subtract after-init-time before-init-time)))
+           gcs-done))
 
-(set-face-attribute 'default nil :font "Fira Code Nerd Font" :height 128)
+(add-hook 'emacs-startup-hook #'emaconf/display-startup-time)
 
-(load-theme 'wombat)
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
-
-;;; Package manager
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -37,101 +40,149 @@
   (load bootstrap-file nil 'nomessage))
 (straight-use-package 'use-package)
 
-;; Configure use-package to use straight.el by default
 (setq straight-use-package-by-default t)
 
-;; theme
-(use-package catppuccin-theme
-  :config
-  (load-theme 'catppuccin :no-confirm)
-  :init
-  (setq catppuccin-flavor 'mocha))
+(use-package el-patch)
 
-;; base config
+(use-package no-littering)
+
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)			; Disable visible scroolbar
+(tool-bar-mode -1)			; Disable the toolbar
+(tooltip-mode -1)			; Disable tooltips
+(set-fringe-mode -1)			; Give some breathing room
+
+(menu-bar-mode -1)			; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
 (column-number-mode)
 (global-display-line-numbers-mode)
-
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook))
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; command log
-(use-package command-log-mode)
-
-(use-package counsel
-  :bind (("C-x b" . counsel-ibuffer)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history)))
-
-(use-package ivy
-  :diminish ivy-mode
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
-  :init
-  (ivy-mode 1)
-  (counsel-mode 1)
+(use-package fontaine
+  :ensure t
+  :when (display-graphic-p)
+  ;; :hook (kill-emacs . fontaine-store-latest-preset)
   :config
-  (setq ivy-use-virtual-buffers t))
+  (setq fontaine-presets
+      '((regular
+           :default-height 140
+           :default-weight regular
+           :fixed-pitch-height 1.0
+           :variable-pitch-height 1.0
+           )
+        (large
+           :default-height 180
+           :default-weight normal
+           :fixed-pitch-height 1.0
+           :variable-pitch-height 1.05
+           )
+        (t
+           :default-family "CaskaydiaCove Nerd Font Mono"
+           :fixed-pitch-family "CaskaydiaCove Nerd Font Mono"
+           :variable-pitch-family "CaskaydiaCove Nerd Font Mono"
+           :italic-family "CaskaydiaCove Nerd Font Mono Italic"
+           :blod-family "CaskaydiaCove Nerd Font Mono Bold"
+           :variable-pitch-weight normal
+           :bold-weight bold
+           :italic-slant italic
+           :line-spacing 0.1)
+        ))
+  ;; (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular))
+  (fontaine-set-preset 'regular)
 
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
+  ;; set emoji font
+  (set-fontset-font
+   t
+   (if (version< emacs-version "28.1")
+       '(#x1f300 . #x1fad0)
+     'emoji)
+   (cond
+    ((member "Noto Emoji" (font-family-list)) "Noto Emoji")
+    ((member "Symbola" (font-family-list)) "Symbola")
+    ((member "Apple Color Emoji" (font-family-list)) "Apple Color Emoji")
+    ((member "Noto Color Emoji" (font-family-list)) "Noto Color Emoji")
+    ((member "Segoe UI Emoji" (font-family-list)) "Segoe UI Emoji")
+    ))
 
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
+  ;; set Chinese font
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font
+     (frame-parameter nil 'font)
+     charset
+     (font-spec :family
+		(cond
+		 ((eq system-type 'darwin)
+		  (cond
+		   ((member "PingFang SC" (font-family-list)) "PingFang SC")
+		   ((member "WenQuanYi Zen Hei" (font-family-list)) "WenQuanYi Zen Hei")
+		   ((member "Microsoft YaHei" (font-family-list)) "Microsoft YaHei")
+		   ))
+		 ((eq system-type 'gnu/linux)
+		  (cond
+		   ((member "WenQuanYi Micro Hei" (font-family-list)) "WenQuanYi Micro Hei")
+		   ((member "Microsoft YaHei" (font-family-list)) "Microsoft YaHei")
+		   ))
+		 (t
+		  (cond
+		   ((member "Sarasa Term SC Nerd" (font-family-list)) "Sarasa Term SC Nerd")
+		   ((member "Microsoft YaHei" (font-family-list)) "Microsoft YaHei")
+		   )))
+		)))
 
-;; M-x all-the-icons-install-fonts
+  ;; set Chinese font scale
+  (setq face-font-rescale-alist `(
+				  ("Symbola"             . 1.3)
+				  ("Microsoft YaHei"     . 1.2)
+				  ("WenQuanYi Zen Hei"   . 1.2)
+				  ("Sarasa Term SC Nerd" . 1.2)
+				  ("PingFang SC"         . 1.16)
+				  ("Lantinghei SC"       . 1.16)
+				  ("Kaiti SC"            . 1.16)
+				  ("Yuanti SC"           . 1.16)
+				  ("Apple Color Emoji"   . 0.91)
+				  ))
+  )
+
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs t)
+
+(load-theme 'modus-vivendi t)
+;; (load-theme 'modus-operandi t)
+
 (use-package all-the-icons)
 
 (use-package doom-modeline
-  :init
-  (doom-modeline-mode 1)
-  :custom
-  ((doom-modeline-height 15)))
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 12)))
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init
-  (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (use-package general
+  :after evil
   :config
-  (general-create-definer rune/leader-keys
+  (general-create-definer emaconf/leader-keys
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
     :global-prefix "C-SPC")
 
-  (rune/leader-keys
+  (emaconf/leader-keys
     "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")
-    "/" '(counsel-rg :which-key "use rg find")
-    )
-  )
+    "tc" '(counsel-load-theme :which-key "choose theme")
+    "tt" '(modus-themes-toggle :which-key "change modus theme")
+    "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.config/emacs/README.org")))))
 
 (use-package evil
   :init
@@ -142,11 +193,11 @@
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  ;; should use it to use shell delete key
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-  
+
   ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
@@ -156,35 +207,132 @@
   :config
   (evil-collection-init))
 
-(use-package hydra)
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 1))
 
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
+(use-package counsel
+  :bind (("C-M-j" . 'counsel-switch-buffer)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (counsel-mode 1))
 
-(rune/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
+(use-package ivy-rich
+  :after ivy counsel
   :init
-  (when (file-directory-p "~/work/codes/projects")
-    (setq projectile-project-search-path '("~/work/codes/projects")))
-  (setq projectile-switch-project-action #'projectile-dired))
+  (ivy-rich-mode 1))
 
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
+(use-package ivy-prescient
+  :after counsel
+  :custom
+  (ivy-prescient-enable-filtering nil)
+  :config
+  (prescient-persist-mode 1)
+  (ivy-prescient-mode 1))
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(defun emaconf/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "JetBrainsMonoNL Nerd Font" :weight 'regular :height (cdr face)))
+  )
+
+(defun emaconf/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(use-package org
+  :commands (org-capture org-agenda)
+  :hook (org-mode . emaconf/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+
+  (emaconf/org-font-setup))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun emaconf/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . emaconf/org-mode-visual-fill))
+
+(with-eval-after-load 'org
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)))
+
+  (push '("conf-unix" . conf-unix) org-src-lang-modes))
 
 (use-package magit
+  :commands magit-status
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(use-package evil-magit
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+(use-package forge
   :after magit)
 
-(provide 'init)
+(use-package evil-nerd-commenter
+  :diminish
+  :bind (("M-/" . evilnc-comment-or-uncomment-lines)
+         :map evil-normal-state-map
+         ("gcc" . evilnc-comment-or-uncomment-lines)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
