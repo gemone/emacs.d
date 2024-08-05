018aea5eac2f3ee9213b5f73aed65124c7b86d81
Gemone <git@gemo.one>
2024-08-06 00:54:49 +0800

    update script: activate pyenv

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 python3 -m venv ./etc/python-venv
-./etc/python-venv/bin/Activate.ps1
+./etc/python-venv/Scripts/Activate.ps1
 pip3 install -U epc orjson sexpdata six setuptools paramiko rapidfuzz
 
 pip3 install -U jedi-language-server
