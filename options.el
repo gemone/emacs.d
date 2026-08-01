b1e217d4a2f7672fcbed5e3369554d605823b350
gemo <git@gemo.one>
2026-08-01 16:35:26 +0800

    refactor: delete all config

--- options.el
+++
@@ -1,8 +0,0 @@
-;;; options.el --- Configuration variable definitions -*- lexical-binding: t; -*-
-
-;; This file is tracked by git and contains default variable definitions.
-;; Use options.local.el to override these values for your local setup.
-
-;; Add your defcustom variables here
-
-;;; options.el ends here
